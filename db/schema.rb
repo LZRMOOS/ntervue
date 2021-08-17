dd5b18a17f2de6582837e9769f3c696b9cbb28ec
Wei Dai <wei.dai@appfolio.com>
2021-08-17 13:41:13 -0700

    Update mime-magic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_202253) do
+ActiveRecord::Schema.define(version: 2020_09_15_174859) do
 
   create_table "car_coolnesses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "score"
